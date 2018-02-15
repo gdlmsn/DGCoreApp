abe6cfdadf4190fe51d1a0c09214bf79e1792d63
Giulio Dalmasson <gdalmasson@gmail.com>
2018-02-15 16:27:17 +1100

    Services - Material

---
+++ ClientApp/app/material.module.ts
@@ -0,0 +1,15 @@
+﻿import { NgModule } from '@angular/core';
+
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+    imports: [MatTabsModule, MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule],
+    exports: [MatTabsModule, MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule],
+
+})
+export class MaterialModule { }
