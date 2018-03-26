7306f41671bd6caa5488b1d3968718c6d679b762
Giulio Dalmasson <gdalmasson@gmail.com>
2018-03-26 16:51:00 +1100

    delete implemented

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -119,4 +119,12 @@ export class VehicleFormComponent implements OnInit {
         }
     }
 
+    delete() {
+        if (confirm("Are you sure")) {
+            this.vehicleService.delete(this.vehicle.id)
+                .subscribe(x => {
+                    this.router.navigate(['/home'])
+                });
+        }
+    }
 }
